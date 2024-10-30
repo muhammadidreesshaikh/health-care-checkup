6609f4ab7a77046ca6f57704b0951427dc94e309
Muhammad Idrees Shaikh <idreesiqbal104@gmail.com>
2024-10-30 15:59:42 +0500

    homepage in online-services section completed

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-
+  onlineServices = [
+    {
+      title: 'Find Doctor',
+      image: 'https://i.ibb.co/jgBMwxM/online1.png',
+      options: [
+        'Please Select',
+        'Cardiology',
+        'Emergency Medicine',
+        'Dental Surgery',
+        'Neurology',
+        'General Physicians',
+        'Dermatology',
+        'ENT',
+        'Hepatology',
+        'Internal Medicine',
+        'Nephrologist'
+      ]
+    },
+    {
+      title: 'Book An Appointment',
+      image: 'https://i.ibb.co/rH1my2Y/calendar.png',
+      options: [
+        'Please Select',
+        'Cardiology',
+        'Emergency Medicine',
+        'Dental Surgery',
+        'Neurology',
+        'General Physicians',
+        'Dermatology',
+        'ENT',
+        'Hepatology',
+        'Internal Medicine',
+        'Nephrologist'
+      ]
+    },
+    {
+      title: 'Online Pharmacy',
+      image: 'https://i.ibb.co/714yWZt/medicine.png',
+      options: [
+        'Please Select',
+        'Cardiology',
+        'Emergency Medicine',
+        'Dental Surgery',
+        'Neurology',
+        'General Physicians',
+        'Dermatology',
+        'ENT',
+        'Hepatology',
+        'Internal Medicine',
+        'Nephrologist'
+      ]
+    }
+  ];
 }
